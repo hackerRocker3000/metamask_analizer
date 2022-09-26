d0170ea8f30b8b2d039c78c13263ced705b07951
dimdof <dimdoff@mail.ru>
2022-09-26 22:19:51 +0300

    add errorFiles array

--- metamask_analizer.py
+++ metamask_analizer.py
@@ -3,51 +3,62 @@ import os
 import re
 
 resultsArr = {}
+errorFiles = []
 
 for (dirPath, dirNames, filenames) in os.walk("."):
     if "Metamask" in dirPath:
         for filename in filenames:
             if re.fullmatch(r'\d{6}\.log', filename):
-                with open(dirPath + '/' + filename, errors='ignore') as f:
-                    content = f.read()
-                    f.close()
+                try:
+                    with open(dirPath + '/' + filename, errors='ignore') as f:
+                        content = f.read()
+                        f.close()
 
-                addressArr = []
-                cachePosition = 0
-                while True:
-                    cachePosition = content.find("cachedBalances", cachePosition)
-                    if cachePosition == -1:
-                        break
-                    cachePosition += 16
-                    quotesPosition = content.find("}}", cachePosition)
-                    addressesJsonWrap = content[cachePosition:quotesPosition] + '}}'
+                    addressArr = []
+                    cachePosition = 0
+                    while True:
+                        cachePosition = content.find("cachedBalances", cachePosition)
+                        if cachePosition == -1:
+                            break
+                        cachePosition += 16
+                        quotesPosition = content.find("}}", cachePosition)
+                        addressesJsonWrap = content[cachePosition:quotesPosition] + '}}'
 
-                    for addressesWrap in json.loads(addressesJsonWrap, strict=False).values():
-                        for address in addressesWrap:
-                            addressArr.append(address)
-                    cachePosition += 1
+                        for addressesWrap in json.loads(addressesJsonWrap, strict=False).values():
+                            for address in addressesWrap:
+                                addressArr.append(address)
+                        cachePosition += 1
 
-                addressArr = list(set(addressArr))
+                    addressArr = list(set(addressArr))
 
-                hashStartPosition = content.find("KeyringController")
-                if hashStartPosition != -1:
-                    hashStartPosition = hashStartPosition + 29
-                    hashEndPosition = content.find("MetaMetricsController", hashStartPosition) - 4
-                    hashRaw = content[hashStartPosition:hashEndPosition]
-                    hashObj = json.loads(hashRaw.replace('\\', ''))
-                    if 'salt' not in hashObj or 'iv' not in hashObj or 'data' not in hashObj:
-                        print("! Invalid hashObj format ...")
-                        exit(1)
-                    metamask = '$metamask$' + hashObj['salt'] + '$' + hashObj['iv'] + '$' + hashObj['data']
+                    hashStartPosition = content.find("KeyringController")
+                    if hashStartPosition != -1:
+                        hashStartPosition = hashStartPosition + 29
+                        hashEndPosition = content.find("MetaMetricsController", hashStartPosition) - 4
+                        hashRaw = content[hashStartPosition:hashEndPosition]
+                        hashObj = json.loads(hashRaw.replace('\\', ''))
+                        if 'salt' not in hashObj or 'iv' not in hashObj or 'data' not in hashObj:
+                            print("! Invalid hashObj format ...")
+                            exit(1)
+                        metamask = '$metamask$' + hashObj['salt'] + '$' + hashObj['iv'] + '$' + hashObj['data']
 
-                    resultsArr[filename] = {
-                        'addresses': addressArr,
-                        'metamask': metamask
-                    }
+                        resultsArr[filename] = {
+                            'addresses': addressArr,
+                            'metamask': metamask
+                        }
+                except Exception:
+                    errorFiles.append(dirPath + '/' + filename)
+
 
 prettyResult = json.dumps(resultsArr, indent=2)
 print(prettyResult)
 with open('result.txt', 'w') as f:
     f.write(prettyResult)
+    f.close()
+
+if len(errorFiles):
+    with open('errorFiles.txt', 'w') as f:
+        f.write("Error files count: " + str(len(errorFiles)) + "\n")
+        f.write(json.dumps(errorFiles))
 
 
